559125340e51f93a4c919ff44183eb25198264b3
kookiben93 <kbenatto@condorcet93.fr>
2025-12-12 14:53:46 +0100

    Implement main method in Main class

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
-# sae
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] tab = new int[8][8];
+        methodes.plateau(tab);
+
+        methodes.remplir(tab);
+
+        for(int i = 0; i < 100; i++){
+            methodes.coordonnees(tab);
+
+            methodes.remplir(tab);
+        }
+    }
+}
